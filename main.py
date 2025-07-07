e0453c6360fae958211af66dc4d944dc898d6124
Doyounggil <tlfkr45@naver.com>
2025-07-08 02:44:51 +0900

    feat: 사용자 생성 기능 구현 및 테스트 완료

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
+from user.interface.controllers.user_controller import router as user_routers
 
 app = FastAPI()
+app.include_router(user_routers)
 
 @app.get("/")
 def hello():
